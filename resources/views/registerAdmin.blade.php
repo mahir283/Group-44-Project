95c047ed2f5e5a7b06684bbe69022ad26161830f
Zeta Nymphaea <black.lotus.zeta@gmail.com>
2024-11-22 01:29:14 +0000

    Register Admin Page

---
+++ resources/views/registerAdmin.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+
+    <link rel="stylesheet" href="{{ asset('css/register.css') }}">
+</head>
+<body>
+<!-- Class container for form-->
+<div class="formContainer">
+
+    <h1>Create Account</h1>
+    <!-- Creating form-->
+    <form method= "post" action = "registerAdmin.blade.php">
+        <div>
+            <!-- Required input firstname text-->
+            <input required type = "text" name = "firstname" id= "fname" placeholder= "First Name" />
+        </div>
+        <div>
+            <!-- Required input lastname text-->
+            <input required type = "text" name = "lastname" id= "lname" placeholder= "Last Name"/>
+        </div>
+        <div>
+            <!-- Required input email using standard email type-->
+            <input required type = "email" name = "email" id= "useremail" placeholder= "Email"/>
+        </div>
+        <div>
+            <!-- Required input username text-->
+            <input required type = "text" name = "username" id= "uname" placeholder= "Username"/>
+        </div>
+        <div>
+            <!-- Required input password text-->
+            <input required type = "password" name = "password" id= "pword" placeholder= "Password"/>
+        </div>
+        <br>
+        <!-- Submit Button-->
+        <input type = "submit" value = "Register"/>
+        <br><br>
+        <input type = "hidden" name = submitted" value = "true"/>
+    </form>
+    <!--linking to other pages of login and user register -->
+    <p> Already a user? <a href="login.user.blade.php"> Login</a> </p>
+    <p> Not a user? <a href="registerUser.blade.php"> Register</a> </p>
+</div>
+
+</body>
+</html>
