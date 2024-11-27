c77583e48e4ea0d911852b3ad2e4dcbf89e3e800
aswv1 <230189809@aston.ac.uk>
2024-11-27 11:17:52 +0000

    changed Contact Page and Home Page

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BrumBrumm</title>
+    <link rel="stylesheet" href="{{ asset("/homepage.css") }}">
+</head>
+<header>
+
+    <a href="{{ url("homepage.html") }}">
+        <img src="{{ asset("/BrumBrumm.png") }}" alt="image" width="150" height="100">
+    </a>
+    <h1></h1>
+    <h1></h1>
+    <h1></h1>
+    <h2 id="carButtonNavBar">Cars</h2>
+    <h1></h1>
+    <h1></h1>
+    <a href="{{ url("ContactPage.html") }}"><h2 id="contactButtonNavBar">Contact</h2></a>
+    <h1></h1>
+    <h1></h1>
+    <h2 id="aboutButtonNavBar">About</h2>
+    <br>
+    <h1></h1>
+    <h1></h1>
+    <h1></h1>
+    <img id="profileImage" src="{{ asset("/profile avatar neww.png") }}" height="75" height="75">
+    <img id="basketImage" src="{{ asset("/basket avatar for nav bar.jpg") }}" height="75" height="75">
+</header>
+
+<div>
+    <img id="BlackSuzuki" src="{{ asset("/black Suzuki Swift-Photoroom.png") }}" height="400" width="600">
+    <img id="BrumBrummBetweenBlackRedCars" src="{{ asset("/BrumBrumm-Photoroom.png") }}" height="250" width="250">
+    <img id="RedMerc" src="{{ asset("/RedMerc-Photoroom.png") }}" height="400" width="600">
+</div>
+<br>
+<div class="SloganHomePage">
+    <p id="YourTimeIsValuable">YOUR TIME IS VALUABLE</p>
+    <p id="FindYourDesiredVehicleOnline">FIND YOUR DESIRED VEHICLE <span id="OnlineWord">ONLINE</span></p>
+    <p id="AndSkipTheWaitAtTheDealership">AND SKIP THE WAIT AT THE DEALERSHIP</p>
+</div>
+
+<div class="ColourSectionHomePage">
+    <h2 id="VERYFAST">VERY FAST <span id="VERYSIMPLE">VERY SIMPLE</span></h2>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+</div>
+<br>
+<br>
+<main>
+    <div>
+        <h1 id="FindYourBrumBrumm">FIND YOUR BRUMBRUMM!</h1>
+    </div>
+    <br>
+    <br>
+    <br>
+    <br>
+    <div>
+        <img id="AudiA42008" src="{{ asset("/audi a4 2008.jpeg") }}">
+        <img id="FordKa2003" src="{{ asset("/ford ka 2003.jpeg") }}">
+        <img id="Yaris2002"src="{{ asset("/toyota yaris 2002.jpeg") }}">
+    </div>
+    <br>
+    <div class="CheckOutMoreButtonContainer">
+        <button id="CheckOutMoreButton">CHECK OUT MORE</button>
+    </div>
+
+</main>
+</html>
