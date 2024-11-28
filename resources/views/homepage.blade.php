22a6fcbca4c4390221f0e3d49f780d34f18d723f
aswv1 <230189809@aston.ac.uk>
2024-11-28 20:10:44 +0000

    changes made to homepage to try sort out css file

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BrumBrumm</title>
-    <link rel="stylesheet" type="text/css" href="{{ asset('public/css/homepage.css') }}" >
 </head>
 <header>
 
