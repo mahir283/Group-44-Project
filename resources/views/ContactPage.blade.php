608cd3b8f3b420e39b4cab405a00c4262763838a
aswv1 <230189809@aston.ac.uk>
2024-11-28 20:26:56 +0000

    changes made to homepage to try sort out css file

--- resources/views/ContactPage.blade.php
+++ resources/views/ContactPage.blade.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="{{ asset("/ContactPage.css") }}">
+    <title>Contact Page</title>
+    <link rel="stylesheet" href="{{ asset("css/ContactPage.css") }}">
 </head>
 <header>
-    <a href="{{ url("homepage.html") }}">
-        <img src="{{ asset("/BrumBrumm.png") }}" alt="image" width="150" height="100">
+    <a href="{{ url("homepage.blade.php") }}">
+        <img src="{{ asset("assets/BrumBrumm.png") }}" alt="image" width="150" height="100">
     </a>
     <h1></h1>
     <h1></h1>
@@ -24,12 +24,12 @@
     <h1></h1>
     <h1></h1>
     <h1></h1>
-    <img id="profileImage" src="{{ asset("/profile avatar neww.png") }}" alt="Profile Picture Image" width="75" height="75">
-    <img id="basketImage" src="{{ asset("/basket avatar for nav bar.jpg") }}" alt="Basket Picture Image" width="75" height="75">
+    <img id="profileImage" src="{{ asset("assets/profile avatar neww.png") }}" alt="Profile Picture Image" width="75" height="75">
+    <img id="basketImage" src="{{ asset("assets/basket avatar for nav bar.jpg") }}" alt="Basket Picture Image" width="75" height="75">
 </header>
 <body>
 <div class="contact">
-    <img id="BrumBrumm" src="{{ asset("/BrumBrumm-Photoroom.png") }}" alt="Logo on Contact Page" height="400" width="450">
+    <img id="BrumBrumm" src="{{ asset("assets/BrumBrumm-Photoroom.png") }}" alt="Logo on Contact Page" height="400" width="450">
     <p id="Mahir">Mahir Afaq</p>
     <p id="MahirEmail">email:123fake@gmail.com</p>
     <p id="MahirNumber">number: 098765432198</p>
