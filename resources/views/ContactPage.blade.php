98224926950e65d1ef08c1f223bb658a5f03c726
aswv1 <230189809@aston.ac.uk>
2024-12-02 21:03:55 +0000

    added a form to contact page

--- resources/views/ContactPage.blade.php
+++ resources/views/ContactPage.blade.php
@@ -28,6 +28,34 @@
     <img id="basketImage" src="{{ asset("assets/basket avatar for nav bar.jpg") }}" alt="Basket Picture Image" width="75" height="75">
 </header>
 <body>
+
+<form>
+    <div>
+        <label>First Name</label>
+        <input type = "text" id = "FirstName" name = "FirstName" placeholder = "First Name" required>
+        <label>Last Name</label>
+        <input type = "text" id = "LastName" name = "LastName" placeholder = "Last Name" required>
+    </div>
+    <br>
+    <div>
+        <label>Email</label>
+        <input type = "Email" id = "Email" name = "Email" placeholder = "Email" required>
+    </div>
+    <br>
+    <div>
+        <label>Phone</label>
+        <input type = "tel" id = "PhoneNumber" name = "PhoneNumber" placeholder = "Phone Number" required>
+    </div>
+    <br>
+    <div>
+        <label>Query</label>
+        <input type = "text" id = "Query" name = "Query" placeholder = "Enter Your Query" required>
+    </div>
+    <br>
+    <div>
+        <input type = "submit">
+    </div>
+</form>
 <div class="contact">
     <img id="BrumBrumm" src="{{ asset("assets/BrumBrumm-Photoroom.png") }}" alt="Logo on Contact Page" height="400" width="450">
     <p id="Mahir">Mahir Afaq</p>
