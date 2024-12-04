d341bd2195c96e260796bca070d1c76c2f758f73
Karamdeep Singh <230180105@aston,ac,uk>
2024-12-04 13:01:36 +0000

    fixed the link for checkout page added about us

---
+++ resources/views/aboutUs.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+    <title>About Us</title>
+    <link rel="stylesheet" href="{{ asset('css/aboutUs.css') }}">
+
+
+</head>
+<body>
+
+
+<div class="wrapper">
+    <div class="row">
+        <div class="image-section">
+            <img src="images/car part 1.jpg" alt="Brum Brumm Motors in action ">
+        </div>
+        <div class="content">
+            <h1>
+                About Us
+            </h1>
+            <h2>
+                Brum Brumm Motors
+            </h2>
+            <p>
+                about us hpw we operate what we would so on so forth
+            </p>
+        </div>
+    </div>
+</div>
+
+
+
+</body>
+
+
+
+
+
+
+
+</html>
