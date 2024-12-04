a1bc894041ba0d8fd454ddf05ca06ba72af10b42
Zeta Nymphaea <black.lotus.zeta@gmail.com>
2024-12-04 12:13:20 +0000

    Testing testing: the navigation bar

--- resources/views/carsPage.blade.php
+++ resources/views/carsPage.blade.php
@@ -9,16 +9,38 @@
 </head>
 <body>
 <header>
+    <!-- Keeping just in case.
 
     <a href="{{ url("/") }}">
         <img src="{{ asset('assets/BrumBrumm.png') }}" alt="image" width="150" height="100">
     </a>
+    <h1></h1>
+    <h1></h1>
+    <h1></h1>
     <a href="{{ url("/products" ) }}"><h2 id="carButtonNavBar">Cars</h2></a>
+    <h1></h1>
+    <h1></h1>
     <a href="{{ url("/contact") }}"><h2 id="contactButtonNavBar">Contact</h2></a>
+    <h1></h1>
+    <h1></h1>
     <h2 id="aboutButtonNavBar">About</h2>
     <br>
+    <h1></h1>
+    <h1></h1>
+    <h1></h1>
     <img id="profileImage" src="{{ asset('assets/profile avatar neww.png') }}" alt="Profile Picture Image" width="75" height="75">
     <img id="basketImage" src="{{ asset('assets/basket avatar for nav bar.jpg') }}" alt="Basket Picture Image" width="75" height="75">
+    -->
+    <div class="navbar">
+        <a href="{{ url("/") }}">
+            <img src="{{ asset('assets/BrumBrumm.png') }}" alt="image" width="150" height="100"></a>
+        <a href="{{ url("/products" ) }}"><h2 id="carButtonNavBar">Cars</h2></a>
+        <a href="{{ url("/contact") }}"><h2 id="contactButtonNavBar">Contact</h2></a>
+        <h2 id="aboutButtonNavBar">About</h2>
+        <br>
+        <img id="profileImage" src="{{ asset('assets/profile avatar neww.png') }}" alt="Profile Picture Image" width="75" height="75">
+        <img id="basketImage" src="{{ asset('assets/basket avatar for nav bar.jpg') }}" alt="Basket Picture Image" width="75" height="75">
+    </div>
 </header>
 
 <!-- SEARCH BAR -->
