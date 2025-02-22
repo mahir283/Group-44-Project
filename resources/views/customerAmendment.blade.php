1cec94df5a3038bb9c6b3bee6211745e02cd278f
Zeta Nymphaea <black.lotus.zeta@gmail.com>
2025-02-22 00:04:18 +0000

    finished customer amendment page

--- resources/views/customerAmendment.blade.php
+++ resources/views/customerAmendment.blade.php
@@ -3,7 +3,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Customer Amendment</title>
-    <link rel="stylesheet" href="{{ asset('css/account.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/customerAmendment.css') }}">
 </head>
 <body>
 
